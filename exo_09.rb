90d7a61d6ee14b385f1946d0e5471b4110765ad0
Kettani <kettani.samir@outlook.fr>
2018-12-30 01:56:37 +0100

    Enfin terminé la journée 4 pour apprendre le ruby !

---
+++ exo_09.rb
@@ -0,0 +1,9 @@
+puts "Bonjour, votre prénom svp ?"
+print "> "
+user_name = gets.chomp
+
+puts "Et votre nom svp ?"
+print"> "
+user_lastname = gets.chomp
+
+puts "Bonjour " + user_name + " " + user_lastname + " !"
