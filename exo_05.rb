3a56ce60d7dbd3066f31237faa563c6ceb60a7e7
Kettani <kettani.samir@outlook.fr>
2018-12-30 01:22:21 +0100

    Enchainement d'exercice continu en ruby

---
+++ exo_05.rb
@@ -0,0 +1,22 @@
+puts "On vas compter le nombre d'heure de travail à THP" # Un simple puts pour afficher un texte
+puts "Travail : #{10 * 5 * 11}" # Un simple puts pour afficher le travail suivi d'un #{} qui permet de faire un calcul apaprt et surrement pour ne pas confondre le texte et le calcul
+puts "En minutes ça fait : #{10 * 5 * 11 * 60 }" #Idem que ligne 2
+
+puts "Et en secondes ?" #idem que ligne 1 sauf qu'ici on pause une question qui est analyser et enregistrer par l'ordinateur
+
+puts 10 * 5 * 11 * 60 * 60 #L'ordinateur effectue le calcul et l'affiche
+
+puts "Est-ce que c'est vrai que 3 + 2 < 5 - 7 ?" #On pause une question qui sera nalyser comme question et sauvegarder dans l'ordinateur
+
+puts 3 + 2 < 5 - 7 #Le calcul est effectué est renvoi une reponse type booleen par apport à la question de la ligne précente.
+
+puts "ça fais combien 3 + 2 ? #{3 + 2}" #idem que ligne 2
+puts "ça fait combien 5 - 7 ? #{5 - 7}" #idem que ligne 2
+
+puts "Ok, c'est faux alors !" #idem que ligne 1
+
+puts "C'est drôle ça, faisons-en plus :" #idem que ligne 1
+
+puts "Est-ce que 5 est plus grand que -2 ? #{5 > -2}" # idem que la ligne 9 et 11 sauf qu'ici c sur une seule ligne. la valeur renvoi un résultat type booleen.
+puts "Est-ce que 5 est supérieur ou égal à -2 ? #{5 >= -2}" #idem
+puts "Est-ce que 5 est inférieur ou égal à -2 ? #{5 <= -2}" #idem
